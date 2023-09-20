bf2b8e2b02b85856d0815fa54de313e533847165
mgjeon <mgjeon@khu.ac.kr>
2023-09-20 19:55:42 +0900

    update

--- Magnetic_Field_Evaluation/series.py
+++ Magnetic_Field_Evaluation/series.py
@@ -1,26 +1,31 @@
-import os 
-import glob
 import argparse
-
+import glob
+import os
 import pickle
-
-from tool.metric import evaluate_single
 
 from tqdm import tqdm
 
+from tool.metric import evaluate_single
+
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--fname', type=str, help='path to nc file',
-                    default='./12673_20170906_000000.nc')
-parser.add_argument('--result_dir', type=str, help='path to results.pickle',
-                    default='./result')
-parser.add_argument('--z_Mm', type=int, help='max height for z-direction [Mm]',
-                    default=40)
+parser.add_argument(
+    "--fname",
+    type=str,
+    help="path to nc file",
+    default="/mnt/obsdata/isee_nlfff_v1.2/12673",
+)
+parser.add_argument(
+    "--result_dir", type=str, help="path to results.pickle", default="./result"
+)
+parser.add_argument(
+    "--z_Mm", type=int, help="max height for z-direction [Mm]", default=40
+)
 
 args = parser.parse_args()
 
 if os.path.isdir(args.fname):
-    files = sorted(glob.glob(os.path.join(args.fname, '*.nc')))
+    files = sorted(glob.glob(os.path.join(args.fname, "*.nc")))
 elif os.path.isfile(args.fname):
     files = [args.fname]
 
@@ -33,6 +38,6 @@ for file in tqdm(files):
 
 results = {k: [r[k] for r in results] for k in results[0].keys()}
 
-result_pickle = os.path.join(args.result_dir, 'result.pickle')
-with open(result_pickle, 'wb') as f:
+result_pickle = os.path.join(args.result_dir, "series_result.pickle")
+with open(result_pickle, "wb") as f:
     pickle.dump(results, f)
