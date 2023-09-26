aa0f2b7b40034b9ae66338443e31858371718d66
mgjeon <mgjeon@khu.ac.kr>
2023-09-26 10:36:19 +0900

    update series py

--- Magnetic_Field_Evaluation/series.py
+++ Magnetic_Field_Evaluation/series.py
@@ -32,30 +32,34 @@ result_pickle = os.path.join(args.result_dir, "series_result.pickle")
 if os.path.exists(result_pickle):
     with open(result_pickle, "rb") as f:
         series_results = pickle.load(f)
-
-    fig_path = os.path.join(args.fig_dir, f"NOAA_{args.noaanum}.png")
-    plot_with_flares(series_results, args.noaanum, fig_path, include_C=False)
-    fig_path = os.path.join(args.fig_dir, f"NOAA_{args.noaanum}_C.png")
-    plot_with_flares(series_results, args.noaanum, fig_path, include_C=True)
+        
+    if len(series_results['date']) > 1:
+        os.makedirs(args.fig_dir, exist_ok=True)
+        fig_path = os.path.join(args.fig_dir, f"NOAA_{args.noaanum}.png")
+        plot_with_flares(series_results, args.noaanum, fig_path, include_C=False)
+        fig_path = os.path.join(args.fig_dir, f"NOAA_{args.noaanum}_C.png")
+        plot_with_flares(series_results, args.noaanum, fig_path, include_C=True)
 else:
     if os.path.isdir(args.fname):
         files = sorted(glob.glob(os.path.join(args.fname, args.ext)))
     elif os.path.isfile(args.fname):
         files = [args.fname]
 
-    os.makedirs(args.result_dir, exist_ok=True)
-
     results = []
 
     for file in tqdm(files):
         results.append(evaluate_single(file, args))
-
+    
     series_results = {k: [r[k] for r in results] for k in results[0].keys()}
+
+    os.makedirs(args.result_dir, exist_ok=True)
 
     with open(result_pickle, "wb") as f:
         pickle.dump(series_results, f)
 
-    fig_path = os.path.join(args.fig_dir, f"NOAA_{args.noaanum}.png")
-    plot_with_flares(series_results, args.noaanum, fig_path, include_C=False)
-    fig_path = os.path.join(args.fig_dir, f"NOAA_{args.noaanum}_C.png")
-    plot_with_flares(series_results, args.noaanum, fig_path, include_C=True)
+    if len(series_results['date']) > 1:
+        os.makedirs(args.fig_dir, exist_ok=True)
+        fig_path = os.path.join(args.fig_dir, f"NOAA_{args.noaanum}.png")
+        plot_with_flares(series_results, args.noaanum, fig_path, include_C=False)
+        fig_path = os.path.join(args.fig_dir, f"NOAA_{args.noaanum}_C.png")
+        plot_with_flares(series_results, args.noaanum, fig_path, include_C=True)
