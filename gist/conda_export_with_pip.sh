7d77d48d7bc78df1738b55b53d94896a123f4457
mgjeon <mgjeon@khu.ac.kr>
2023-10-05 10:17:00 +0900

    add ref to gist sh

--- gist/conda_export_with_pip.sh
+++ gist/conda_export_with_pip.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# https://github.com/conda/conda/issues/9628#issuecomment-1608913117
 # Extract installed pip packages
 pip_packages=$(conda env export | grep -A9999 ".*- pip:" | grep -v "^prefix: ")
 
